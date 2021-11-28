76834c201e3012f0252ec3f850e68a8371432d19
Andrey Korolev <hoolm-2012@yandex.ru>
2021-11-29 01:13:15 +0500

    d

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ document.getElementById('btn_clr').addEventListener('click', function () {
 let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "pi"];
 let operations = ["+", "-", "*", "/", "=", "√"];
 
-number.forEach(element => {
-  document.getElementById('btn_' + element).addEventListener('click', function () {
-  	if (String(element) !== 'pi'){
-  		inputWindow.value += String(element);
-  	}
-  	else{
-  		inputWindow.value += Math.PI;
-  	}
-  	history.value += String(element);
-});
-});
-
+if(number) {
+	number.forEach(element => {
+		document.getElementById('btn_' + element).addEventListener('click', function () {
+			if (String(element) !== 'pi') {
+				inputWindow.value += String(element);
+			} else {
+				inputWindow.value += Math.PI;
+			}
+			history.value += String(element);
+		});
+	});
+}
 operations.forEach(element => {
 	document.getElementById('btn_' + element).addEventListener('click', function () {
 		 if (String(element) !== '=' && String(element) !== '√' && String(element) !== '-') {
